76f29a8dd1e06b3166dcd5e33c432d4a98c12704
sjsj1703 <89244254+sjsj1703@users.noreply.github.com>
2024-12-27 20:13:18 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -0,0 +1,9 @@
+
+def solution(array, height):
+    answer=0
+    for i in range(len(array)):
+        if height<array[i]:
+            answer+=1
+        else:
+            continue
+    return answer
